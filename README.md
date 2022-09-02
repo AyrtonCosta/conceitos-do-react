3184cbcd6a99bd63864dadec868ed01906fa74ff
unknown <ayrtondsp30@gmail.com>
2022-09-02 11:44:34 -0300

    projeto final

---
+++ README.md
@@ -0,0 +1,17 @@
+<h5>Projeto desenvolvido no curso IGNITE de REACTJS da RocketSeat.</h5>
+Nesse módulo, <strong>criamos uma aplicação React utilizando o Vite e aprenderemos sobre os conceitos mais importantes do React</strong>, entre eles estão <strong>componentização, propriedades, estados, imutabilidade e hooks, além de aplicar o TypeScript no nosso projeto</strong> para adicionar tipagem estática à aplicação.
+
+## 🧪 Tecnologias
+
+- [ReactJS](https://javascript.org/)
+- [TypeScript](https://javascript.org/)
+- [HTML](https://html.org/)
+- [CSS](https://css.org/)
+
+![gif](./src/assets/gif.gif)
+![gif](./src/assets/asset.png)
+![gif](./src/assets/assets.png)
+
+---
+
+<p align="center">Made with ♥ by Ayrton Costa</p>
---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { SideBar } from "./components/SideBar";
+
+import styles from "./App.module.css";
+import "./global.css";
+
+export function App() {
+  const posts = [
+    {
+      id: 1,
+      author: {
+        avatarUrl: "https://github.com/maykBrito.png",
+        name: "Mayrk Brito",
+        role: "Instrutor",
+      },
+      content: [
+        { type: "paragraph", content: "Fala galeraa 👋👋" },
+        {
+          type: "paragraph",
+          content:
+            "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+        },
+        { type: "link", content: "jane.design/doctorcare" },
+      ],
+      publishedAt: new Date("2022-08-03 20:00:00"),
+    },
+    {
+      id: 2,
+      author: {
+        avatarUrl: "https://github.com/diego3g.png",
+        name: "Diego",
+        role: "CTO",
+      },
+      content: [
+        { type: "paragraph", content: "Fala galeraa 👋👋" },
+        {
+          type: "paragraph",
+          content:
+            "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+        },
+        { type: "link", content: "jane.design/doctorcare" },
+      ],
+      publishedAt: new Date("2022-08-10 20:00:00"),
+    },
+  ];
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
